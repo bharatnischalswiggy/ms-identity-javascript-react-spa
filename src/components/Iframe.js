be6098342d2781e711ea7e690e764d2ea8c82b0a
Bharat Nischal <bharat.nischal@swiggy.in>
2021-11-29 13:18:15 +0530

    srcDoc approach for iframe

--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const Iframe = () => {
+    const [content, setContent] = useState('<div>Loading....</div>');
     const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        // axios({
+        //     url: 'https://www.google.com',
+        //     method: 'get',
+        //     headers: {
+
+        //     }
+        // })
+        // .then(res => {
+        //     console.log(res.data);
+        // })
+        fetch(`https://localhost:8088/superset/welcome#${token}`, {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                'X-custom-token': token,
+                'Cache-Control': 'no-cache',
+                'Content-Type': 'text/html',
+            }
+        })
+        .then(response => response.text())
+        .then(html => {
+            console.log("🚀 ~ file: Iframe.js ~ line 18 ~ useEffect ~ html", html)
+            setContent(html);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }, []);
+
     return (
-        <iframe src={`http://localhost:8088/superset/welcome#${token}`} height="500" width="100%" title="Iframe Example" />
+        <iframe srcDoc={content} height="500" width="100%" title="Iframe Example" />
     )
 }
 
